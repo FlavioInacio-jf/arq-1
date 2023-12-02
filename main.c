1fa3bb4dcfdd1d541b40a89ba1469d7adcaefb33
inacio <inacio@pop-os.localdomain>
2023-12-02 12:32:00 -0300

    refactor: Add decodificador de instrução

--- main.c
+++ main.c
@@ -18,6 +18,8 @@
 
 // Load memory vector from a file
 void loadMemory(FILE *input, uint8_t *mem8, uint32_t *mem32);
+void decodeInstruction(uint8_t *executa, uint32_t registers[SIZE_MEMORY], uint8_t *mem8, uint32_t *mem32);
+void decodeInstructions(uint32_t registers[SIZE_MEMORY], uint8_t *mem8, uint32_t *mem32);
 
 // Principal function
 int main(int argc, char *argv[])
@@ -41,6 +43,7 @@ int main(int argc, char *argv[])
   uint32_t *mem32 = (uint32_t *)(calloc(SIZE_MEMORY, 1024));
 
   loadMemory(input, mem8, mem32);
+  decodeInstructions(registers, mem8, mem32);
 
   fclose(input);
   return 0;
@@ -70,4 +73,108 @@ void loadMemory(FILE *input, uint8_t *mem8, uint32_t *mem32)
 
     count += 1;
   }
+}
+
+void decodeInstruction(uint8_t *executa, uint32_t registers[SIZE_MEMORY], uint8_t *mem8, uint32_t *mem32)
+{
+
+  char instrucao[30] = {0};
+
+  uint8_t z = 0, x = 0, i = 0;
+  uint32_t pc = 0, xyl = 0;
+
+  registers[28] = ((mem8[registers[29] + 0] << 24) | (mem8[registers[29] + 1] << 16) | (mem8[registers[29] + 2] << 8) | (mem8[registers[29] + 3] << 0)) | mem32[registers[29] >> 2];
+
+  uint8_t opcode = (registers[28] & (0b111111 << 26)) >> 26;
+  switch (opcode)
+  {
+  // mov
+  case 0b000000:
+    // Obtendo operandos
+    z = (registers[28] & (0b11111 << 21)) >> 21;
+    xyl = registers[28] & 0x1FFFFF;
+    // Execucao do comportamento
+    registers[z] = xyl;
+    // Formatacao da instrucao
+    sprintf(instrucao, "mov r%u,%u", z, xyl);
+    // Formatacao de saida em tela (deve mudar para o arquivo de saida)
+    printf("0x%08X:\t%-25s\tR%u=0x%08X\n", registers[29], instrucao, z, xyl);
+    break;
+  // l8
+  case 0b011000:
+    // Otendo operandos
+    z = (registers[28] & (0b11111 << 21)) >> 21;
+    x = (registers[28] & (0b11111 << 16)) >> 16;
+    i = registers[28] & 0xFFFF;
+    // Execucao do comportamento com MEM8 e MEM32
+    registers[z] = mem8[registers[x] + i] | (((uint8_t *)(mem32))[(registers[x] + i) >> 2]);
+    // Formatacao da instrucao
+    sprintf(instrucao, "l8 r%u,[r%u%s%i]", z, x, (i >= 0) ? ("+") : (""), i);
+    // Formatacao de saida em tela (deve mudar para o arquivo de saida)
+    printf("0x%08X:\t%-25s\tR%u=MEM[0x%08X]=0x%02X\n", registers[29], instrucao, z, registers[x] + i, registers[z]);
+    break;
+  // l32
+  case 0b011010:
+    // Otendo operandos
+    z = (registers[28] & (0b11111 << 21)) >> 21;
+    x = (registers[28] & (0b11111 << 16)) >> 16;
+    i = registers[28] & 0xFFFF;
+    // Execucao do comportamento com MEM8 e MEM32
+    registers[z] = ((mem8[((registers[x] + i) << 2) + 0] << 24) | (mem8[((registers[x] + i) << 2) + 1] << 16) | (mem8[((registers[x] + i) << 2) + 2] << 8) | (mem8[((registers[x] + i) << 2) + 3] << 0)) | mem32[registers[x] + i];
+    // Formatacao da instrucao
+    sprintf(instrucao, "l32 r%u,[r%u%s%i]", z, x, (i >= 0) ? ("+") : (""), i);
+    // Formatacao de saida em tela (deve mudar para o arquivo de saida)
+    printf("0x%08X:\t%-25s\tR%u=MEM[0x%08X]=0x%08X\n", registers[29], instrucao, z, (registers[x] + i) << 2, registers[z]);
+    break;
+  // bun
+  case 0b110111:
+    // Armazenando o PC antigo
+    pc = registers[29];
+    // Execucao do comportamento
+    registers[29] = registers[29] + ((registers[28] & 0x3FFFFFF) << 2);
+    // Formatacao da instrucao
+    sprintf(instrucao, "bun %i", registers[28] & 0x3FFFFFF);
+    // Formatacao de saida em tela (deve mudar para o arquivo de saida)
+    printf("0x%08X:\t%-25s\tPC=0x%08X\n", pc, instrucao, registers[29] + 4);
+    break;
+  // int
+  case 0b111111:
+    // Parar a execucao
+    (*executa) = 0;
+    // Formatacao da instrucao
+    sprintf(instrucao, "int 0");
+    // Formatacao de saida em tela (deve mudar para o arquivo de saida)
+    printf("0x%08X:\t%-25s\tCR=0x00000000,PC=0x00000000\n", registers[29], instrucao);
+    break;
+  // Instrucao desconhecida
+  default:
+    // Exibindo mensagem de erro
+    printf("Instrucao desconhecida!\n");
+    // Parar a execucao
+    (*executa) = 0;
+  }
+  // PC = PC + 4 (proxima instrucao)
+  registers[29] = registers[29] + 4;
+
+  // Exibindo a finalizacao da execucao
+}
+
+void decodeInstructions(uint32_t registers[SIZE_MEMORY], uint8_t *mem8, uint32_t *mem32)
+{
+  printf("\nMEM8:\n");
+  for (uint8_t i = 0; i < 48; i = i + 4)
+    printf("0x%08X: 0x%02X 0x%02X 0x%02X 0x%02X\n", i, mem8[i], mem8[i + 1], mem8[i + 2], mem8[i + 3]);
+
+  printf("\nMEM32:\n");
+  for (uint8_t i = 0; i < 12; i = i + 1)
+    printf("0x%08X: 0x%08X (0x%02X 0x%02X 0x%02X 0x%02X)\n", i << 2, mem32[i], ((uint8_t *)(mem32))[(i << 2) + 3], ((uint8_t *)(mem32))[(i << 2) + 2], ((uint8_t *)(mem32))[(i << 2) + 1], ((uint8_t *)(mem32))[(i << 2) + 0]);
+
+  printf("\nSaida esperada\n\n      |       \n      V       \n\n");
+  printf("[START OF SIMULATION]\n");
+
+  uint8_t executa = 1;
+  while (executa)
+    decodeInstruction(&executa, registers, mem8, mem32);
+
+  printf("[END OF SIMULATION]\n");
 }
