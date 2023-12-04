6a6300df845fd370ac27005111a57165dd6566dc
inacio <inacio@pop-os.localdomain>
2023-12-04 15:46:27 -0300

    refactor: Feito a instrução ble

--- main.c
+++ main.c
@@ -593,7 +593,21 @@ void ble(uint32_t registers[NUM_REGISTERS], FILE *output)
   // Fetch operands
   const uint32_t label = registers[IR] & 0x03FFFFFF;
 
-  // Falta fazer
+  // Instruction formatting
+  sprintf(instruction, "ble %i", label);
+
+  // Execution of behavior
+  const uint32_t oldPC = registers[PC];
+  if (isZNSet(registers) || (isSNSet(registers) != isOVSet(registers)))
+  {
+    registers[PC] = registers[PC] + (label << 2);
+  }
+
+  // Screen output formatting
+  printf("0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
+
+  // Output formatting to file
+  fprintf(output, "0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
 }
 
 void blt(uint32_t registers[NUM_REGISTERS], FILE *output)
