37cb4acc34ec989d5f26ae3e9c6d01eed4b05293
inacio <inacio@pop-os.localdomain>
2023-12-04 15:30:26 -0300

    refactor: Feito a instrução bae

--- main.c
+++ main.c
@@ -401,7 +401,21 @@ void bae(uint32_t registers[NUM_REGISTERS], FILE *output)
   // Fetch operands
   const uint32_t label = registers[IR] & 0x03FFFFFF;
 
-  // Falta fazer
+  // Instruction formatting
+  sprintf(instruction, "bae %i", label);
+
+  // Execution of behavior
+  const uint32_t oldPC = registers[PC];
+  if (!isCYSet(registers))
+  {
+    registers[PC] = registers[PC] + (label << 2);
+  }
+
+  // Screen output formatting
+  printf("0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
+
+  // Output formatting to file
+  fprintf(output, "0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
 }
 
 void bat(uint32_t registers[NUM_REGISTERS], FILE *output)
