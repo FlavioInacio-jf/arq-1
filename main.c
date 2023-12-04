28a7937e08b764a07fa8144f7b36bda3da1fb991
inacio <inacio@pop-os.localdomain>
2023-12-04 15:36:19 -0300

    refactor: Feito a instrução bbe

--- main.c
+++ main.c
@@ -449,7 +449,21 @@ void bbe(uint32_t registers[NUM_REGISTERS], FILE *output)
   // Fetch operands
   const uint32_t label = registers[IR] & 0x03FFFFFF;
 
-  // Falta fazer
+  // Instruction formatting
+  sprintf(instruction, "bbe %i", label);
+
+  // Execution of behavior
+  const uint32_t oldPC = registers[PC];
+  if (isZNSet(registers) || isCYSet(registers))
+  {
+    registers[PC] = registers[PC] + (label << 2);
+  }
+
+  // Screen output formatting
+  printf("0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
+
+  // Output formatting to file
+  fprintf(output, "0x%08X:\t%-25s\tPC=0x%08X\n", oldPC, instruction, registers[PC] + 4);
 }
 
 void bbt(uint32_t registers[NUM_REGISTERS], FILE *output)
