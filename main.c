3e4ce234a6bd1fd7980d8a685b9cd9acf3fec254
inacio <inacio@pop-os.localdomain>
2023-12-15 18:37:45 -0300

    refactor: Ajuste mul

--- main.c
+++ main.c
@@ -489,41 +489,36 @@ void sub(uint32_t registers[NUM_REGISTERS], FILE *output)
 
 void mul(uint32_t registers[NUM_REGISTERS], FILE *output)
 {
-  char instruction[30] = {0};
-
   // Fetch operands
   const uint8_t z = (registers[IR] >> 21) & 0x1F;
   const uint8_t x = (registers[IR] >> 16) & 0x1F;
   const uint8_t y = (registers[IR] >> 11) & 0x1F;
   const uint8_t l = registers[IR] & 0x1F;
 
-  // Instruction formatting
-  sprintf(instruction, "mul %s,%s,%s,%s",
-          formatRegisterName(l, true), formatRegisterName(z, true), formatRegisterName(x, true), formatRegisterName(y, true));
-
   // Execution of behavior
-  const uint64_t valueX = (uint64_t)registers[x];
-  const uint64_t valueY = (uint64_t)registers[y];
+  const uint32_t valueX = registers[x];
+  const uint32_t valueY = registers[y];
 
   const uint64_t result = valueX * valueY;
   registers[z] = (uint32_t)result;
   registers[l] = (result >> 32) & 0xFFFFFFFF;
 
   if (result == 0)
-  {
     registers[SR] |= ZN_FLAG;
-  }
 
   if (registers[l] != 0)
-  {
     registers[SR] |= CY_FLAG;
-  }
 
-  // Screen output formatting
-  printf("0x%08X:\t%-25s\t%s:%s=%s*%s=0x%016lX,SR=0x%08X\n", registers[PC], instruction, formatRegisterName(l, false), formatRegisterName(z, false), formatRegisterName(x, false), formatRegisterName(y, false), result, registers[SR]);
+  // Instruction formatting
+  char instruction[30] = {0};
+  char additionalInfo[50] = {0};
 
-  // Output formatting to file
-  fprintf(output, "0x%08X:\t%-25s\t%s=%s+%s=0x%016lX,SR=0x%08X\n", registers[PC], instruction, formatRegisterName(z, false), formatRegisterName(x, false), formatRegisterName(y, false), result, registers[SR]);
+  sprintf(instruction, "mul %s,%s,%s,%s",
+          formatRegisterName(l, true), formatRegisterName(z, true), formatRegisterName(x, true), formatRegisterName(y, true));
+  sprintf(additionalInfo, "%s:%s=%s*%s=0x%016lX,SR=0x%08X", formatRegisterName(l, false), formatRegisterName(z, false), formatRegisterName(x, false), formatRegisterName(y, false), result, registers[SR]);
+
+  // Output
+  printInstruction(registers[PC], output, instruction, additionalInfo);
 }
 
 void sll(uint32_t registers[NUM_REGISTERS], FILE *output)
