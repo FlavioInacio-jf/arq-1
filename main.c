3c1a7179e613b5a5de82af30ae6efbedc67db8d6
inacio <inacio@pop-os.localdomain>
2023-12-16 14:56:19 -0300

    refactor: Ajuste  addi

--- main.c
+++ main.c
@@ -1021,19 +1021,12 @@ void xor (uint32_t registers[NUM_REGISTERS], FILE *output) {
   printInstruction(registers[PC], output, instruction, additionalInfo);
 }
 
-    void addi(uint32_t registers[NUM_REGISTERS], FILE *output)
+void addi(uint32_t registers[NUM_REGISTERS], FILE *output)
 {
-  char instruction[30] = {0};
-
   // Fetch operands
   const uint8_t z = (registers[IR] >> 21) & 0x1F;
   const uint8_t x = (registers[IR] >> 16) & 0x1F;
-  const uint8_t i = (registers[IR] & 0xFFFF) |
-                    ((registers[IR] & 0x00004000) ? 0xFC000000 : 0x00000000);
-
-  // Instruction formatting
-  sprintf(instruction, "addi %s,%s,%u",
-          formatRegisterName(z, true), formatRegisterName(x, true), i);
+  const int32_t i = extendSign(registers[IR] & 0xFFFF, 16);
 
   // Execution of behavior
   const uint64_t valueX = (uint64_t)registers[x];
@@ -1041,31 +1034,38 @@ void xor (uint32_t registers[NUM_REGISTERS], FILE *output) {
   const uint64_t result = valueX + (uint64_t)i;
   registers[z] = (uint32_t)result;
 
-  if (registers[z] == 0)
-  {
+  if (result == 0)
     registers[SR] |= ZN_FLAG;
-  }
+  else
+    registers[SR] &= ~ZN_FLAG;
 
-  if ((result & 0x80000000)) // Check MSB
-  {
+  if ((result & 0x80000000))
     registers[SR] |= SN_FLAG;
-  }
+  else
+    registers[SR] &= ~SN_FLAG;
 
-  if ((valueX > 0 && i > 0 && result <= 0) || (valueX < 0 && i < 0 && result >= 0))
-  {
+  if (
+      ((valueX & 0x80000000) == (i & 0x80000000)) &&
+      ((result & 0x80000000) != (valueX & 0x80000000)))
     registers[SR] |= OV_FLAG;
-  }
+  else
+    registers[SR] &= ~OV_FLAG;
 
   if (result > 0xFFFFFFFF)
-  {
     registers[SR] |= CY_FLAG;
-  }
+  else
+    registers[SR] &= ~CY_FLAG;
 
-  // Screen output formatting
-  printf("0x%08X:\t%-25s\t%s=%s+0x%08X=0x%08X,SR=0x%08X\n", registers[PC], instruction, formatRegisterName(z, false), formatRegisterName(x, false), i, registers[z], registers[SR]);
+  // Instruction formatting
+  char instruction[30] = {0};
+  char additionalInfo[60] = {0};
 
-  // Output formatting to file
-  fprintf(output, "0x%08X:\t%-25s\t%s=%s+0x%08X=0x%08X,SR=0x%08X\n", registers[PC], instruction, formatRegisterName(z, false), formatRegisterName(x, false), i, registers[z], registers[SR]);
+  sprintf(instruction, "addi %s,%s,%i",
+          formatRegisterName(z, true), formatRegisterName(x, true), i);
+  sprintf(additionalInfo, "%s=%s+0x%08X=0x%08X,SR=0x%08X", formatRegisterName(z, false), formatRegisterName(x, false), i, registers[z], registers[SR]);
+
+  // Output
+  printInstruction(registers[PC], output, instruction, additionalInfo);
 }
 
 void subi(uint32_t registers[NUM_REGISTERS], FILE *output)
